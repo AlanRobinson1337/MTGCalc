5b32a03e6f93b03510f3a7a3c4e97bb56762bb2c
Alan <Alan@DESKTOP-BJDSL35>
2019-04-23 20:43:02 +0100

    Database Logic

---
+++ src/mtglandcalc/Math.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mtglandcalc;
+
+/**
+ *
+ * @author Alan
+ */
+public class Math {
+    int a; int b; int c; int d; int e;
+    int total;
+    int deck;
+    int mana;
+    
+    public void setDeck(int deck){
+        this.deck = deck;
+    }
+    public void setMana(int mana){
+        this.mana = mana;
+    }
+    
+    public double one(int a){
+        return a;
+    }
+    
+    public double two(int a, int b){
+        return a;
+    }
+    
+    public double three(int a, int b, int c){
+        return a;
+    }
+    
+    public double four(int a, int b, int c, int d){
+        return a;
+    }
+    public double five(int a, int b, int c, int d, int e){
+        return a;
+    }
+}
+    
